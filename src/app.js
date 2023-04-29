d4f174fcfb5b3b8422af729cbb357763395c31f6
Rasheedat Jinadu <jinaduleoluwa@gmal.com>
2023-04-29 22:02:30 +0100

    fix: cookie animation and position fixed

--- src/app.js
+++ src/app.js
@@ -1,40 +1,47 @@
-let hamburgerMenu = document.querySelector(".menu");
-let hamBtn = document.querySelector(".hamburger");
+let hamburgerMenu = document.querySelector('.menu');
+let hamBtn = document.querySelector('.hamburger');
 
-hamBtn.addEventListener("click", () => {
-  hamburgerMenu.classList.toggle("active");
-  hamBtn.classList.toggle("open");
+hamBtn.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active');
+  hamBtn.classList.toggle('open');
 });
 
-let header = document.querySelector("header");
+let header = document.querySelector('header');
 window.onscroll = () => {
-  header.classList.add("active");
+  header.classList.add('active');
 };
 
-window.addEventListener("scroll", function () {
+window.addEventListener('scroll', function () {
   if (document.documentElement.scrollTop === 0) {
-    header.classList.remove("active");
+    header.classList.remove('active');
   }
 });
 
-let cookieBox = document.querySelector(".cookie");
-let button = document.querySelectorAll(".disappear");
+let cookieBox = document.querySelector('.cookie');
+let button = document.querySelectorAll('.disappear');
+let logo = document.querySelector('.logo');
 
 button.forEach((accept) =>
-  accept.addEventListener("click", () => {
-    cookieBox.classList.add("slideout");
+  accept.addEventListener('click', () => {
+    cookieBox.classList.add('slideout');
   })
 );
 
-var swiper = new Swiper(".mySwiper", {
+logo.addEventListener('click', () => {
+  cookieBox.style.display = 'block';
+});
+
+setTimeout(() => logo.click(), 5000);
+
+var swiper = new Swiper('.mySwiper', {
   loop: true,
   centeredSlides: true,
   spaceBetween: 20,
-  direction: "vertical",
+  direction: 'vertical',
   slidesPerView: 1.7,
   speed: 1000,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
   autoplay: {
